560c8344b132ad864ad4b1afc8c34d5eaa836146
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-01-21 23:27:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 
-num1 = input("Enter a number : ")
-num2= input("Enter another number : ")
-result = float(num1) + float(num2)
+# num1 = input("Enter a number : ")
+# num2= input("Enter another number : ")
+# result = float(num1) + float(num2)
 
-print(result)
+# print(result)
+
+### building a better calc
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator:  ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
+
+
 
